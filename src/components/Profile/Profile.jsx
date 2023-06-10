8c891ddc646698014d713c6ecc00afe0ec8fa3b6
Paweł Brzozowski <pawel@brzoza.net>
2023-06-10 21:08:28 +0200

    Minor CSS fixes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,20 +8,20 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       <div className={styles.description}>
         <img src={avatar} alt={username} className={styles.avatar} />
         <p className={styles.name}>{username}</p>
-        <p className={styles.tag}>{tag}</p>
+        <p className={styles.tag}>@{tag}</p>
         <p className={styles.location}>{location}</p>
       </div>
       <ul className={styles.stats}>
         <li>
-          <span className={styles.label}>Followers</span>
+          <span className={styles.label}>Followers: </span>
           <span className={styles.quantity}>{stats.followers}</span>
         </li>
         <li>
-          <span className={styles.label}>Views</span>
+          <span className={styles.label}>Views: </span>
           <span className={styles.quantity}>{stats.views}</span>
         </li>
         <li>
-          <span className={styles.label}>Likes</span>
+          <span className={styles.label}>Likes: </span>
           <span className={styles.quantity}>{stats.likes}</span>
         </li>
       </ul>
