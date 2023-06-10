60a49facc021054e0e7f708403186bb48fe2881c
Paweł Brzozowski <pawel@brzoza.net>
2023-06-10 20:00:37 +0200

    Create Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import styles from "./Profile.module.css";
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className={styles.profile}>
+    <section className={styles.profile}>
       <div className={styles.description}>
         <img src={avatar} alt={username} className={styles.avatar} />
         <p className={styles.name}>{username}</p>
@@ -25,7 +25,7 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
           <span className={styles.quantity}>{stats.likes}</span>
         </li>
       </ul>
-    </div>
+    </section>
   );
 };
 
